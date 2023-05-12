cf2f125811113b46aeee3b3179736aaaad689ba7
kalet chavez <kalet.svdev@gmail.com>
2023-05-11 23:20:51 -0600

    commit 003

--- src/App/App.js
+++ src/App/App.js
@@ -9,25 +9,25 @@ import React from 'react';
 
 const defaultRutine=[
   {id:1,musculo: 'pecho', ejercicios: [
-    {ejercicio:'press banca', repeticiones:'4 x 12',done:true},
-    {ejercicio:'press inclinado', repeticiones:' 4 x 20',done:false}
+    {idItem:'A1', ejercicio:'press banca', repeticiones:'4 x 12',done:true},
+    {idItem:'A2', ejercicio:'press inclinado', repeticiones:' 4 x 20',done:false}
   ],dia: 'Martes',completed:false},
   {id:2,musculo: 'pierna', ejercicios: [
-    {ejercicio:'sentadilla vulgara ', repeticiones:'4 x 12',done:false},
-    {ejercicio:' sentadilla con barra ',repeticiones:'4 x 20',done:false},
+    {idItem:'B2', ejercicio:'sentadilla vulgara ', repeticiones:'4 x 12',done:false},
+    {idItem:'B3', ejercicio:' sentadilla con barra ',repeticiones:'4 x 20',done:false},
   ],dia: 'Jueves',completed:false},
   {id:3,musculo: 'espalda', ejercicios: [
-    {ejercicio:'sentadilla vulgara', repeticiones:'4 x 12',done:false},
-    {ejercicio:' sentadilla con barra', repeticiones: '4 x 20',done:false},
-    {ejercicio:'sentadilla vulgara', repeticiones: '4 x 12',done:false},
-    {ejercicio:' sentadilla con barra', repeticiones: '4 x 20',done:false}
+    {idItem:'C1', ejercicio:'sentadilla vulgara', repeticiones:'4 x 12',done:false},
+    {idItem:'C2', ejercicio:' sentadilla con barra', repeticiones: '4 x 20',done:false},
+    {idItem:'C3', ejercicio:'sentadilla vulgara', repeticiones: '4 x 12',done:false},
+    {idItem:'C4', ejercicio:' sentadilla con barra', repeticiones: '4 x 20',done:false}
   ],dia: 'Jueves',completed:false},
   {id:4,musculo: 'espalda', ejercicios: [
-    {ejercicio:'sentadilla vulgara', repeticiones:'4 x 12',done:false},
-    {ejercicio:' sentadilla con barra', repeticiones: '4 x 20',done:false},
-    {ejercicio:'sentadilla vulgara', repeticiones: '4 x 12',done:false},
-    {ejercicio:' sentadilla con barra', repeticiones: '4 x 20',done:false}
-  ],dia: 'Jueves',completed:true}
+    {idItem:'D1', ejercicio:'sentadilla vulgara', repeticiones:'4 x 12',done:false},
+    {idItem:'D2', ejercicio:' sentadilla con barra', repeticiones: '4 x 20',done:false},
+    {idItem:'D3', ejercicio:'sentadilla vulgara', repeticiones: '4 x 12',done:false},
+    {idItem:'D4', ejercicio:' sentadilla con barra', repeticiones: '4 x 20',done:false}
+  ],dia: 'Jueves',completed:false}
 ]
 
 const colors = [
@@ -44,15 +44,76 @@ function App() {
   const [serchValue,setSerchValue] = React.useState('')
   const [changeColor,setChangeColor] = React.useState(colors[0].color)
   const [changeTextColor,setChangeTextColor] = React.useState(colors[0].letra)
-  const [completed,setCompleted] = React.useState(false)
+  const [completed,setCompleted] = React.useState(defaultRutine)
+  const [done,setDone] = React.useState(defaultRutine)
+  const [deleteCheck,setDeleteCheck] = React.useState(defaultRutine)
+  const [deleted,setDeleted] = React.useState(defaultRutine)
 
-  const serchRutine = defaultRutine.filter((value)=> {
+  const serchRutine = completed.filter((value)=> {
     const serchedRutine = value.musculo.toLowerCase()
     const inputValue = serchValue.toLowerCase()
     return serchedRutine.includes(inputValue)
   })
-  
+
  
+  
+  const complete = (id)=>{
+    const newRutine = [...completed]
+
+    let lengthDone =0;
+    newRutine.forEach((e)=>{
+      if(e.id === id){
+        lengthDone = e.ejercicios.length
+      }
+    })
+    const index = newRutine.findIndex((e)=> e.id === id)
+
+    let counter =0;
+    newRutine[index].ejercicios.map((e)=>{
+      if(e.done === true){
+        counter++
+      }
+    })
+    if(lengthDone === counter){
+      newRutine[index].completed = true
+      newRutine.splice(index,1)
+      setCompleted(newRutine)
+    }
+    console.log(completed)
+  }
+
+  const onCheck = (idItem,idRutine)=>{
+    const newRutineItem = [...defaultRutine]
+    newRutineItem.map((e)=>{
+      if(e.id === idRutine){
+        e.ejercicios.map((e,i)=>{
+         if(e.idItem == idItem){
+          e.done = true
+         }
+        })
+      }
+    })
+    setDone(newRutineItem)
+    complete(idRutine)
+  }
+
+  const onCheckDelete = (idItem,idRutine)=>{
+    const newRutineItem = [...defaultRutine]
+    newRutineItem.map((item)=>{
+      if(item.id === idRutine){
+        const index = item.ejercicios.findIndex((e)=>e.idItem === idItem)
+         item.ejercicios.splice(index,1)
+      }
+    })
+    setDeleteCheck(newRutineItem)
+    complete(idRutine)
+  }
+ 
+  const onCompleted = ()=>{
+    const newRutineTodo = [...defaultRutine]
+
+
+  }
   
   return (
    <>
@@ -70,6 +131,9 @@ function App() {
    <ListRutine>
     {serchRutine.map((rutineItem)=>
       <ItemRutine
+        rutine={rutineItem.id}
+        onCheck={onCheck}
+        onCheckDelete={onCheckDelete}
         setCompleted={setCompleted}
         completed={rutineItem.completed}
         letra={changeTextColor}
--- src/ItemRutine/index.js
+++ src/ItemRutine/index.js
@@ -1,20 +1,28 @@
 import './ItemRutine.css'
 
-export function ItemRutine({musculo,ejercicios,dia,letra,completed,setCompleted,changeColor}){
+export function ItemRutine({musculo,ejercicios,dia,letra,rutine,onCheck,onCheckDelete,changeColor}){
+
    return (
        <div style={{backgroundColor: changeColor}} className='Item' >
          <h2 style={{color:letra}}> Dia de {musculo} </h2>
          <p style={{color:letra}} className='Day-Item'>Dia de entreno {dia}</p>
          <ul style={{color:letra}} className='Item-Rutine'>
             {ejercicios.map((ejercicio)=>(
-            <li style={{color:letra}}>
+            <li 
+            key={ejercicio.idItem}
+            style={{color:letra}}>
                <span 
                onClick={()=>{
-                  ejercicio.done = true
+                  onCheck(ejercicio.idItem,rutine)
                }}
                className='Item-Completed'>V </span>
-               <p className={`${ejercicio.done && "Item-Rutine-completed"}`} style={{color:letra}}> {ejercicio.ejercicio} series: {ejercicio.repeticiones}</p> 
-               <span className='Item-Eliminated'>X</span>
+               <p className={`Item-Ejercicios ${ejercicio.done && "Item-Rutine-completed"}`} style={{color:letra}}>{ejercicio.ejercicio} </p>
+               <p className={`Item-Series ${ejercicio.done && "Item-Rutine-completed"}`} style={{color:letra}}> series: {ejercicio.repeticiones}</p> 
+               <span 
+               onClick={()=>{
+                  onCheckDelete(ejercicio.idItem,rutine)
+               }}
+               className='Item-Eliminated'>X</span>
             </li>)
            )
            }
